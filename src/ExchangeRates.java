5e657a768df710e4f07b6fde0dd840a7bc026687
jorginio <Skata200>
2018-09-25 17:07:18 +0100

    rates1

--- src/ExchangeRates.java
+++ src/ExchangeRates.java
@@ -9,7 +9,7 @@ public class ExchangeRates {
 
     public static void main(String args[]){
 
-        float euros,eurosTotal;
+        float euros,eurosTotal=0;
         float rate = 1.1159f;
 
 
@@ -19,17 +19,15 @@ public class ExchangeRates {
         System.out.print("Please enter amount to be exchanged in Euros: ");
         euros = Integer.parseInt(input.nextLine());
 
+
+
     while(euros>0){
 
-        eurosTotal = euros*rate;
+         eurosTotal = euros*rate;
+    }
+        String result = String.format("The total exchange amount is  %.2f  Euros ",eurosTotal );
+        System.out.printf(result);
 
-        System.out.format("The total exchange amount is  %f.%n  Euros ",eurosTotal );
 
     }
-
-
-    }
-
-
-
 }
